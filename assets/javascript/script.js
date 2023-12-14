cedf333d01d9c3d1e965e6b831672ec2a0954797
Ryan Hemlock <ryan@ryanhemlock.com>
2023-12-14 08:44:36 -0500

    Fetch API properly working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,48 @@
 // Initialized variables
-let city;
 
 // API URLs
 const APIkey = 'd706a8baa5538ab15ced6f4891dbff96';
-const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`;
 
 // HTML Elements
 const mainWeatherSearch = $('#primary-weather-search');
 const citySearchInput = $('#main-city-input');
 
-// Fetching API data
-async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data[0]);
-}
+// Fetch API Data
+const getData = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // Rethrow the error if needed
+    }
+};
 
 // Event listener for main city search form
-mainWeatherSearch.on('submit', function (event) {
+mainWeatherSearch.on('submit', (event) => {
     event.preventDefault();
-    
-    city = citySearchInput.val().toLowerCase();
-    console.log(city)
+
+    let city = citySearchInput.val().toLowerCase();
+    let geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`;
+    console.log(`city: ${city}`)
+
+    // Fetching city location geocode data
+    getData(geoUrl)
+        .then((data) => {
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+
+            console.log(`Lat: ${lat}`)
+            console.log(`Lon: ${lon}`)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
+
 })
 
-//getData(geoUrl)
+const getCurrentWeather = async () => {
+    const response = await fetch(geoUrl);
+    const data = await response.json();
+}
