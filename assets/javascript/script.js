bb3cb21f6d5d120962b6ab675557eee6de513347
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-12-14 07:18:36 -0500

    Updated javascript with keys

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,27 @@
-const APIkey = process.env.WEATHER_API_KEY
-const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=philadelphia&appid=${APIkey}`
+// Initialized variables
+let city;
 
-console.log(APIkey);
+// API URLs
+const APIkey = 'd706a8baa5538ab15ced6f4891dbff96';
+const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIkey}`;
+
+// HTML Elements
+const mainWeatherSearch = $('#primary-weather-search');
+const citySearchInput = $('#main-city-input');
+
+// Fetching API data
+async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data[0]);
+}
+
+// Event listener for main city search form
+mainWeatherSearch.on('submit', function (event) {
+    event.preventDefault();
+    
+    city = citySearchInput.val().toLowerCase();
+    console.log(city)
+})
+
+//getData(geoUrl)
